b2794edb170a952efb341e481b41af5859477e5b
shadmansaalim <shadmansaalim999@gmail.com>
2023-07-17 12:29:22 +0600

    Project file structuring and routing setup

---
+++ src/routes/routes.tsx
@@ -0,0 +1,28 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+]);
+
+export default routes;
