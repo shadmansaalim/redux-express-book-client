86cf71cc566fbccf32545403b38dea0ad4fd5c8c
shadmansaalim <shadmansaalim999@gmail.com>
2023-07-19 21:50:08 +0600

    Logout feature integrated and header links toggle

--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -2,13 +2,26 @@ import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
 import profile from "../../assets/profile.png";
 import "./_Header.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faUser,
-  faUserTie,
-  faRightFromBracket,
-} from "@fortawesome/free-solid-svg-icons";
+import { faUser, faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+import { useAppDispatch, useAppSelector } from "../../redux/hooks";
+import { signOut } from "@firebase/auth";
+import { setUser } from "../../redux/features/users/userSlice";
+import { auth } from "../../lib/firebase";
 
 export default function Header() {
+  const { user } = useAppSelector((state) => state.users);
+
+  const dispatch = useAppDispatch();
+
+  const handleLogout = () => {
+    console.log("Logout");
+    signOut(auth)
+      .then(() => {
+        dispatch(setUser(null));
+      })
+      .catch((error) => {});
+  };
+
   return (
     <Navbar expand="lg">
       <Container>
@@ -37,21 +50,35 @@ export default function Header() {
               Announcements
             </Nav.Link>
 
-            <NavDropdown
-              title={<img src={profile} alt="" width="40" height="40" />}
-              id="profile-dropdown"
-            >
-              <NavDropdown.Item eventKey="4.0" disabled>
-                Name
-              </NavDropdown.Item>
-              <NavDropdown.Item eventKey="4.1">
-                <FontAwesomeIcon className="me-2" icon={faUser} /> Profile
-              </NavDropdown.Item>
-              <NavDropdown.Item eventKey="4.2">
-                <FontAwesomeIcon className="me-2" icon={faRightFromBracket} />{" "}
-                Logout
-              </NavDropdown.Item>
-            </NavDropdown>
+            {user.email ? (
+              <NavDropdown
+                title={<img src={profile} alt="" width="40" height="40" />}
+                id="profile-dropdown"
+              >
+                <NavDropdown.Item eventKey="4.0" disabled>
+                  Name
+                </NavDropdown.Item>
+                <NavDropdown.Item eventKey="4.1">
+                  <FontAwesomeIcon className="me-2" icon={faUser} /> Profile
+                </NavDropdown.Item>
+                <NavDropdown.Item eventKey="4.2" onClick={handleLogout}>
+                  <FontAwesomeIcon className="me-2" icon={faRightFromBracket} />{" "}
+                  Logout
+                </NavDropdown.Item>
+              </NavDropdown>
+            ) : (
+              <>
+                <Nav.Link
+                  className="text-dark me-0 me-lg-2 my-0"
+                  href="/signup"
+                >
+                  Signup
+                </Nav.Link>
+                <Nav.Link className="text-dark me-0 me-lg-2 my-0" href="/login">
+                  Login
+                </Nav.Link>
+              </>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>
